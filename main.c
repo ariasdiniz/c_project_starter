8784270d0798273aebb11e0bc920eaab6a616a25
Aria Diniz <aria.diniz.dev@gmail.com>
2024-02-11 21:37:08 -0300

    defining cmake file

--- main.c
+++ main.c
@@ -14,7 +14,40 @@
                "structure for a new C project.\n\n"\
                "Params: --name: name of the project.\n"\
                "Example: cgen --name my_project\n"
+
+  #define MAIN "int main(int argc, char *argv[]) {\n"\
+               "  return 0;\n"\
+               "}\n"
+
+  #define CMAKE "cmake_minimum_required(VERSION 3.15)\n"\
+                "\n"\
+                "project(%s)\n"\
+                "\n"\
+                "set(CMAKE_C_STANDARD %s)\n"\
+                "set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O3")\n"\
+                "\n"\
+                "file(GLOB_RECURSE SOURCE \"src/*.c\" \"src/*.h\")\n"\
+                "file(GLOB_RECURSE LIB \"src/*.c\" \"src/*.h\")\n"\
+                "file(GLOB_RECURSE INCLUDE \"src/*.c\" \"src/*.h\")\n"\
+                "\n"\
+                "add_executable(\n"\
+                                "%s\n"\
+                                "main.c\n"\
+                                "${SOURCE}\n"\
+                                "${LIB}\n"\
+                                "${INCLUDE})\n"\
+                "\n"\
+                "include_directories(src)\n"\
+                "include_directories(lib)\n"\
+                "include_directories(include)\n"\
+
 #endif
+
+static void make_dir(char *dir_name, struct stat *st) {
+  if (stat(dir_name, st) == -1) {
+    mkdir(dir_name, 0755);
+  }
+}
 
 int main(int argc, char* argv[]) {
   if (argc == 1) {
@@ -25,9 +58,13 @@ int main(int argc, char* argv[]) {
   HashTable *args = parse_args(argc, argv);
 
   if (strcmp(getfromhash(args, "--help"), "\0") || strcmp(getfromhash(args, "-h"), "\0")) {
+    deletehash(args);
     printf(HELP);
     exit(0);
   }
-  
+
+  struct stat st = {0};
+
+  deletehash(args);
   return 0;
 }
